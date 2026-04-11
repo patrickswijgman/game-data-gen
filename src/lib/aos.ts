608dc8bd24c6afdcc733a52db0856da9435b199c
Patrick <petrik09@live.nl>
2026-04-11 16:18:28 +0200

    fix: aos length

--- src/lib/aos.ts
+++ src/lib/aos.ts
@@ -10,7 +10,7 @@ export function addArrayOfStructures(header: string, output: Array<string>) {
 
 function addArrayOfStructuresDefinition(name: string, struct: string, length: string, output: Array<string>) {
   output.push(`/** An array of ${capitalize(struct)} objects (structures). */`);
-  output.push(`export const ${name} = new Array<${capitalize(struct)}>(length)`);
+  output.push(`export const ${name} = new Array<${capitalize(struct)}>(${length})`);
   output.push(`for (let i=0; i<${length}; i++) {`);
   output.push(`  ${name}[i] = create${capitalize(struct)}()`);
   output.push("}");
--- tests/data.ts
+++ tests/data.ts
@@ -1,5 +1,5 @@
 /*
- * Generated with game-data-gen on 4/11/2026, 4:16:24 PM. DO NOT MODIFY THIS FILE!
+ * Generated with game-data-gen on 4/11/2026, 4:18:15 PM. DO NOT MODIFY THIS FILE!
  */
 
 /*
@@ -85,7 +85,7 @@ export function zeroEntity(obj: Entity) {
 export const MAX_ENTITIES_COUNT = 2048
 
 /** An array of Entity objects (structures). */
-export const entities = new Array<Entity>(length)
+export const entities = new Array<Entity>(2048)
 for (let i=0; i<2048; i++) {
   entities[i] = createEntity()
 }
